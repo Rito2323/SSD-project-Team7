2377fea618d62a4bed028c0e118a7dbb42ac6506
Vishal <40630630+vishmishra8199@users.noreply.github.com>
2021-11-24 16:56:05 +0530

    Checking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import {  Table } from "semantic-ui-react";
+
+const sample =[ 
+  {
+  
+    "Survey_No": 1,
+    "Created_By" : "Gargi@students.iiit.ac.in",
+    "Participants" : "Visha;@students.iiit.ac.in",
+    "Answers" : {
+      "Q1":"A1",
+      "Q2":"A2",
+      "Q3":"A3",
+      "Q4":"A4"
+      }
+    },
+    {
+    "Survey_No": 1,
+    "Created_By" : "Gargi@students.iiit.ac.in",
+    "Participants" : "Yash@students.iiit.ac.in",
+    "Answers" : {
+      "Q1":"A1",
+      "Q2":"A2",
+      "Q3":"A3",
+      "Q4":"A4"
+    }  
+  }
+  
+  
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   /* <div className="App">
+      <p>Survey_NO  : {sample.Responses.Survey_No}</p>
+      <p>Created By  : {sample.Responses.Created_By}</p>
+      <p>Answers By  : {sample.Responses.Participants}</p>
+      <p>Ans1  : {sample.Responses.Answers.Q1}</p>
+      <p>Ans2  : {sample.Responses.Answers.Q2}</p>
+      <p>Ans3  : {sample.Responses.Answers.Q3}</p>
+      <p>Ans4  : {sample.Responses.Answers.Q4}</p>
+    </div>*/
+
+    <Table singleLine>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Survey_No.</Table.HeaderCell>
+            <Table.HeaderCell>Created_by</Table.HeaderCell>
+            <Table.HeaderCell>Participant</Table.HeaderCell>
+            <Table.HeaderCell>Question1</Table.HeaderCell>
+            <Table.HeaderCell>Question2</Table.HeaderCell>
+            <Table.HeaderCell>Question3</Table.HeaderCell>
+            <Table.HeaderCell>Question4</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {sample.map(el => {
+            return (
+              <Table.Row key={el.Survey_No}>
+                <Table.Cell>{el.Survey_No}</Table.Cell>
+                <Table.Cell>
+                  {el.Created_By}
+                  </Table.Cell>
+                  <Table.Cell>
+                   {el.Participants}
+                </Table.Cell>
+                <Table.Cell>{el.Answers.Q1}</Table.Cell>
+                <Table.Cell>{el.Answers.Q2}</Table.Cell>
+                <Table.Cell>{el.Answers.Q3}</Table.Cell>
+                <Table.Cell>{el.Answers.Q4}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
   );
 }
 
